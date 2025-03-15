1f2a9cc4779a369b69469aab0986ed46dc4d7750
seafretxx <118317084+seafretxx@users.noreply.github.com>
2025-03-15 14:33:00 +0100

    Drag_Drop

---
+++ Assets/DeckBonsy/Scripts/SC_DragDrop.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SC_Roman_Elite : MonoBehaviour
+{
+    private bool IsDragging = false;
+    
+    void Start()
+    {
+        
+    }
+
+    public void StartDrag()
+    {
+        IsDragging = true;
+    }
+    
+    public void EndDrag()
+    {
+        IsDragging = false;
+    }
+
+    void Update()
+    {
+        if (IsDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        }
+    }
+}
