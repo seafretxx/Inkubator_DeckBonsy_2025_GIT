70f88c0f0def66e616055e66d545d2d2b34f9440
seafretxx <118317084+seafretxx@users.noreply.github.com>
2025-03-17 13:09:07 +0100

    gowno

--- Assets/DeckBonsy/Scripts/SC_DragDrop.cs
+++ Assets/DeckBonsy/Scripts/SC_DragDrop.cs
@@ -1,32 +1,50 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class SC_Roman_Elite : MonoBehaviour
 {
-    private GameObject Canvas;
-    
     private bool IsDragging = false;
-    
+    private Vector3 startPosition;
+    private Transform startParent;
+
     void Start()
     {
-       
+        startParent = transform.parent; // Zapisujemy pocz¹tkowego rodzica (rêka)
     }
 
     public void StartDrag()
     {
         IsDragging = true;
+        startPosition = transform.position;
     }
-    
+
     public void EndDrag()
     {
         IsDragging = false;
+
+        GameObject dropZone = GameObject.Find("DropZone"); // ZnajdŸ DropZone
+        if (dropZone != null && IsOverDropZone(dropZone))
+        {
+            transform.SetParent(dropZone.transform, false); // Ustawienie karty w DropZone
+            transform.position = dropZone.transform.position; // Blokowanie pozycji karty
+        }
+        else
+        {
+            transform.position = startPosition; // Jeœli nie trafi³a w DropZone, wraca na rêkê
+        }
     }
 
     void Update()
     {
         if (IsDragging)
         {
-            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
-          
+            transform.position = Input.mousePosition; // Karta porusza siê za myszk¹
         }
+    }
+
+    private bool IsOverDropZone(GameObject dropZone)
+    {
+        RectTransform dropRect = dropZone.GetComponent<RectTransform>();
+        return RectTransformUtility.RectangleContainsScreenPoint(dropRect, Input.mousePosition);
     }
 }
--- Assets/DeckBonsy/Scripts/SC_DrawCards.cs
+++ Assets/DeckBonsy/Scripts/SC_DrawCards.cs
@@ -3,39 +3,31 @@ using System.Collections.Generic;
 
 public class SC_DrawCards : MonoBehaviour
 {
-    public List<GameObject> deckCards = new List<GameObject>(); // Talia kart (widoczna w Inspectorze)
-    public GameObject Hand; // Obszar reprezentuj¹cy rêkê (np. panel UI)
+    public GameObject[] deckCards; // Talia kart (ró¿ne prefabry)
+    public GameObject Hand;
 
-    private List<GameObject> handCards = new List<GameObject>(); // Lista kart na rêce
-    private int maxCardsInHand = 3; // Maksymalna liczba kart na rêce
+    private List<GameObject> handCards = new List<GameObject>();
+    private int maxCardsInHand = 3;
 
     public void OnClick()
     {
-        if (handCards.Count >= maxCardsInHand) // Sprawdzenie, czy mo¿na dobraæ kartê
+        if (handCards.Count >= maxCardsInHand)
         {
             Debug.Log("Masz ju¿ maksymaln¹ liczbê kart na rêce!");
             return;
         }
 
-        if (deckCards.Count == 0) // Jeœli talia jest pusta
+        if (deckCards.Length == 0)
         {
-            Debug.Log("Brak kart w talii!");
+            Debug.Log("Talia jest pusta!");
             return;
         }
 
-        // Losowanie karty z talii
-        int randomIndex = Random.Range(0, deckCards.Count);
-        GameObject randomCardPrefab = deckCards[randomIndex];
-
-        // Tworzenie karty i dodanie jej na rêkê
-        GameObject card = Instantiate(randomCardPrefab, new Vector2(0, 0), Quaternion.identity);
-        card.transform.SetParent(Hand.transform, false);
+        int randomIndex = Random.Range(0, deckCards.Length);
+        GameObject card = Instantiate(deckCards[randomIndex], Hand.transform, false);
         handCards.Add(card);
 
-        // Usuniêcie karty z talii (opcjonalnie)
-        deckCards.RemoveAt(randomIndex);
-
-        Debug.Log("Dobra³eœ kartê: " + randomCardPrefab.name + ". Aktualna liczba kart na rêce: " + handCards.Count);
+        Debug.Log("Dobra³eœ kartê. Aktualna liczba kart: " + handCards.Count);
     }
 
     public void RemoveCard(GameObject card)
@@ -43,7 +35,6 @@ public class SC_DrawCards : MonoBehaviour
         if (handCards.Contains(card))
         {
             handCards.Remove(card);
-            Destroy(card);
             Debug.Log("Usuniêto kartê. Aktualna liczba kart: " + handCards.Count);
         }
     }
