b30a42a1b19168c8d6afaa52f69d0386986643dd
seafretxx <118317084+seafretxx@users.noreply.github.com>
2025-03-30 14:12:42 +0200

    punkty na karcie

--- Assets/DeckBonsy/Scripts/CardContainer.cs
+++ Assets/DeckBonsy/Scripts/CardContainer.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
-
+using TMPro;
 public class CardContainer : MonoBehaviour
 {
     private Card cardInfo;
@@ -8,7 +8,8 @@ public class CardContainer : MonoBehaviour
     public int points;
 
     [SerializeField] private int handIndex;
-
+    [SerializeField] private TMP_Text pointsText;
+    [SerializeField] private TMP_Text nameText;
     public Card GetCardInfo()
     {
         return cardInfo;
@@ -17,6 +18,25 @@ public class CardContainer : MonoBehaviour
     public void SetCardInfo( Card _cardInfo)
     {
         cardInfo = _cardInfo;
+        cardType = _cardInfo.cardType;
+        points = _cardInfo.points;
+
+        if (pointsText != null)
+        {
+            pointsText.text = points.ToString();
+        }
+        else
+        {
+            Debug.LogWarning("pointsText is not assigned!");
+        }
+        if (nameText != null)
+        { 
+            nameText.text = _cardInfo.cardName;
+        }
+        else
+        {
+            Debug.LogWarning("nameText is not assigned!");
+        }
     }
     public void SetHandIndex(int _handIndex)
     {
@@ -27,5 +47,20 @@ public class CardContainer : MonoBehaviour
     {
         Debug.Log("YAY!");
         GameManager.gameManager.SetChosenCardIndex(handIndex);
+    }
+
+    private void Awake()
+    {
+        if (pointsText == null)
+            pointsText = GetComponentInChildren<TMP_Text>();
+        if (nameText == null)
+        {
+            TMP_Text[] allTexts = GetComponentsInChildren<TMP_Text>();
+            foreach (var text in allTexts)
+            {
+                if (text.gameObject.name.Contains("NameTekst")) 
+                    nameText = text;
+            }
+        }
     }
 }
